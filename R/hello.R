0ab6c7bcefd3aa8eef7b1d950c002caacb31c6d1
Gabriel Belle <g.belle@4intelligence.com.br>
2022-06-28 11:42:04 -0300

    ADD: estrutura do pkg

---
+++ R/hello.R
@@ -0,0 +1,23 @@
+#' @title Hello World - função teste para estrutura
+#' @name hello
+#'
+#' @description ---
+#'
+#' @param x ---
+#'
+#' @author Gabriel Bellé
+#'
+#' @details ---
+#'
+#' @return O retorno é ---
+#'
+#' @examples
+#' \dontrun{
+#' hello(x)
+#' }
+#'
+#' @export
+
+hello <- function(x) {
+  print(paste0("Hello, world!", x))
+}
