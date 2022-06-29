ab714f6cb7e38a7a59a13e7787b662327b08d7aa
Gabriel Belle <g.belle@4intelligence.com.br>
2022-06-29 10:15:37 -0300

    ADD: fct to expand time frame in df

---
+++ R/expand_series.R
@@ -0,0 +1,42 @@
+#' @title Expand date span in a time series
+#' @name expand_series
+#'
+#' @description Aumenta o vetor de datas de um dataframe, inputando NAs na coluna de observação,
+#' para ser preenchida na função de forecast.
+#'
+#' @param df DataFrame contendo uma linha com data e observação da série;
+#' @param end_projection Date informando quando a projeção encerra.
+#'
+#' @author Gabriel Bellé
+#'
+#' @details O input deve ser um df contendo pelo as colunas:
+#' \code{date}: Data da observação:
+#' \code{vl}: valor da observação.
+#'
+#' @return Retorna o mesmo df de input, mas com o período de datas aumentado de acordo com
+#' o fim da projeção fornecida.
+#'
+#' @examples
+#' \dontrun{
+#' expand_series(df = df_series,
+#'               end_projection = '2026-12-01')
+#' }
+#'
+#' @export
+
+expand_series <- function(df, end_projection, periodicity = 'month') {
+
+  n_months = ifelse(periodicity == 'month', 1,
+                    ifelse(periodicity == 'quarter', 3,
+                           stop('Erro na periodicidade! Escolha "month" ou "quarter')))
+
+  df <- df %>%
+    bind_rows(
+      tibble(date = seq(max(df$date) %m+% months(n_months), #Lubridate
+                        as.Date(end_projection),
+                        by = periodicity)
+             )
+    )
+
+  return(df)
+}
