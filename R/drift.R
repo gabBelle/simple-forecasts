610d2591b4e5867bc9f7be19a2856bac241c7c18
Gabriel Belle <g.belle@4intelligence.com.br>
2022-07-04 19:49:11 -0300

    FIX: descricao e chamada das fcts auxs

--- R/drift.R
+++ R/drift.R
@@ -1,25 +1,29 @@
 #' @title Drift forecast
 #' @name drift
 #'
-#' @description Realiza a projeção de uma série usando tendência naive.
+#' @description Realiza a projeção de uma tendência naive, a qual pode ser acoplada em uma projeção.
+#'
+#' Ex: drift naive, repete o último valor e soma ou multiplica o valor do drift_forecast; ou,
+#' seasonal naive with drift, repete o último valor do mesmo mês e soma ou multiplica o valor do drift_forecast.
+#'
 #' @author Gabriel Bellé
 #'
 #' @param df Dataframe contendo a série a ser projetada;
 #' @param end_projection Data indicando fim da projeção;
 #' @param nmean Opcional, constante numérica;
 #' @param manual_drift Opcional, vetor de valores numéricos indicando drift em %;
-#' @param target_value Opcional, vetor de valores indicando a projeção desejada para final de período
+#' @param target_value Opcional, vetor de valores indicando a projeção desejada para final de período.
 #'
 #' @details
 #' O @param df de entrada deve conter pelo as colunas de:
 #' \code{date}: Data da observação:
 #' \code{vl}: valor da observação.
 #'
-#' O @param nmean indica quantos anos do histórico serão utilizados para calcular a tendência linear.
+#' @param nmean indica quantos anos do histórico serão utilizados para calcular a tendência linear.
 #' Se nenhum valor fornecido, utilizará o histórico completo.
 #'
-#' O @param manual_drift deve conter um vetor numérico onde cada constante
-#' indica a variação mensal desejada na projeção para cada ano, caso o tamanho seja menor que
+#' @param manual_drift deve conter um vetor numérico onde cada constante
+#' indica a variação mensal desejada na projeção para cada ano. Caso o tamanho seja menor que
 #' a quantidade de anos a ser projetada, repetirá-se o último valor colocado.
 #' Ex: manual_drift = c(0.1, 0.1, 0.15, 0.2).
 #' Assim, será utilizado 0.1% nos dois primeiros anos, 0.15% no terceiro, 0.2% no quarto e nos anos seguintes,
@@ -27,7 +31,7 @@
 #'
 #' Atenção: este parâmetro faz com que a tendência seja exponencial. O aumento em t+1 é uma % do valor em t + valor em t.
 #'
-#' O @param target_value indica o valor para o final de período desejado, idealmete advindo de uma projeção anual.
+#' @param target_value indica o valor para o final de período desejado, idealmete advindo de uma projeção anual.
 #' Por exemplo, a projeção anual do LatamFocus aponta 150 para 2023 e 200 para 2024, pode-se preencher:
 #' end_projection = '2025-12-01' e target_value = c(150, 200)
 #'
