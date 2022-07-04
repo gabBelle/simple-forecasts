1306810da3ab29352b04f51e46944a29937091cc
Gabriel Belle <g.belle@4intelligence.com.br>
2022-07-04 19:48:32 -0300

    FIX: checa apenas usando cada valor do vetor para um ano

--- R/check_input_len.R
+++ R/check_input_len.R
@@ -8,6 +8,7 @@
 #'
 #' @param df_forecast Dataframe contendo a série com o período de projeção calculado;
 #' @param vector_to_check Vetor contendo o alvo do drift ou os valores do drift manual.
+#' @param n_months Opcional, indica por quantos meses cada variação no vetor manual_drift será utilizado. Para série trimestral, quantos trimestres.
 #'
 #' @details
 #' O @param df_forecast de entrada deve conter pelo as colunas de:
@@ -29,21 +30,27 @@
 check_vector_len <- function(df_forecast,
                              vector_to_check) {
 
+  #Qtd de anos para serem projetados (completos e incompletos)
   n_years <- df_forecast %>%
-    dplyr::filter(is.na(vl)) %>%
+    dplyr::filter(forecast) %>%
+    mutate(year = format(date, '%Y')) %>%
     purrr::pluck('year') %>%
     dplyr::n_distinct()
 
-  if(length(vector_to_check) > n_years) {
-    warning('Para estimar a tendência foi fornecido um vetor maior que o necessário! O excesso será truncado.')
+  if(n_years > length(vector_to_check)) {
+    warning('Para estimar a tendencia foi fornecido um vetor maior que o necessario! O excesso sera truncado.')
 
     vector_to_check <- vector_to_check[1:n_years]
 
-  } else if (length(vector_to_check) < n_years) {
+  } else if (n_years < length(vector_to_check)) {
+
     last_value <- vector_to_check[-1]
     vector_to_check <- vector_to_check[1:n_years]
     vector_to_check <- ifelse(is.na(vector_to_check), last_value, vector_to_check)
+
+  } else {
+    vector_to_check <- vector_to_check
   }
 
-  return(complete_vector)
+  return(vector_to_check)
 }
