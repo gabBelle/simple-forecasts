9bf86d3511e1d63e94db065830aae8d1146b08ca
Gabriel Belle <g.belle@4intelligence.com.br>
2022-07-07 15:31:05 -0300

    ADD: opção de retornar o historico da razão qnd o df_dessaz não tiver projeção

--- R/seas_ratio.R
+++ R/seas_ratio.R
@@ -4,17 +4,15 @@
 #' @description Realiza a projeção de uma série original usando a projeção de uma série dessaz
 #' fazendo a compatibilização pela razão histórica entre a série original e a dessaz.
 #'
+#' Caso seja fornecido uma série dessaz sem projeção, o retorno passa a ser apenas o fator sazonal histórico médio.
+#'
 #' @author Gabriel Bellé
 #'
 #' @param df_original Dataframe de entrada com a série original;
 #' @param df_dessaz Dataframe de entrada com a série dessaz e a projeção já realizada até horizonte desejado;
 #' @param nmeans Opcional, número de anos que será utilizado do histórico para as computações.
 #'
-#' @details O input deve ser um df contendo pelo as colunas de:
-#' \code{date}: Data da observação:
-#' \code{vl}: valor da observação.
-#'
-#' @return O retorno é um df contendo os valores da série projetada e a data.
+#' @return Pode ser o mesmo df_original com a projeção ou apenas o histórico média do fator sazonal.
 #'
 #' @examples
 #' \dontrun{
@@ -27,39 +25,65 @@
 
 seas_ratio <- function(df_original, df_dessaz, nmeans = NULL) {
 
-  if(max(df_dessaz$date) <= max(df_original$date)) {
-    stop('ERRO: o df dessaz deve ter projeção em relação ao original!')
-  }
-
   df_ratio = df_original %>%
     dplyr::rename(original = vl) %>%
     dplyr::right_join(df_dessaz %>%
                  dplyr::rename(dessaz = vl)) %>%
-    dplyr::mutate(ratio = original / dessaz)
+    dplyr::mutate(
+      forecast = ifelse(is.na(original), T, F),
+      ratio = original / dessaz)
 
-  if(is.null(nmeans)) {
-    hist_mean <- df_ratio
-  } else{
+  if (is.null(nmeans)) {
+    date_filt = df_ratio %>%
+      dplyr::filter(!forecast) %>%
+      dplyr::mutate(
+        year = format(date, '%Y') %>%
+          as.numeric(),
+        month = format(date, '%m') %>%
+          as.numeric()
+      )
+
+    month_last_date <- date_filt %>%
+      dplyr::filter(date == max(date)) %>%
+      purrr::pluck('month')
+
+    date_filt <- date_filt %>%
+      dplyr::filter(year == min(year)) %>%
+      dplyr::filter(month == month_last_date) %>%
+      purrr::pluck('date')
+
     hist_mean <- df_ratio %>%
-      dplyr::filter(date >= max(date) - years(nmeans))
+      dplyr::filter(date >= date_filt)
+
+  } else {
+    hist_mean <- df_ratio %>%
+      filter(!forecast) %>%
+      dplyr::filter(date >= max(date)- years(nmeans))
   }
 
   hist_mean <- hist_mean %>%
-    dplyr::group_by(month = format(date, '%B')) %>%
+    dplyr::group_by(month = format(date, '%m'),
+                    month = as.numeric(month)) %>%
     dplyr::summarise(ratio_mean = mean(ratio, na.rm = T)) %>%
     dplyr::ungroup()
 
-  df_forecast <- df_ratio %>%
-    dplyr::mutate(month = format(date, '%B')) %>%
-    dplyr::left_join(hist_mean) %>%
-    dplyr::mutate(
-      forecast = is.na(original),
-      original = ifelse(is.na(original),
-                        dessaz * ratio_mean,
-                        original)
-    ) %>%
-    dplyr::rename(vl = original) %>%
-    dplyr::select(date, vl, forecast)
+  if(max(df_dessaz$date) <= max(df_original$date)) {
+    warning('Como não há projeção no df_dessaz em relação ao df_original, o retorno é a média histórica do fator sazonal.')
 
-  return(df_forecast)
+    output <- hist_mean
+  } else {
+    output <- df_ratio %>%
+      dplyr::mutate(month = format(date, '%m'),
+                    month = as.numeric(month)) %>%
+      dplyr::left_join(hist_mean) %>%
+      dplyr::mutate(
+        original = ifelse(forecast,
+                          dessaz * ratio_mean,
+                          original)
+      ) %>%
+      dplyr::rename(vl = original) %>%
+      dplyr::select(date, vl, forecast)
+  }
+
+  return(output)
 }
