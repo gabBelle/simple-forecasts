76aa06a277811e1acdcac3282d40cc9bd1118555
Gabriel Belle <g.belle@4intelligence.com.br>
2022-07-08 18:40:35 -0300

    ADD: fct convert yoy to target vl

---
+++ R/get_target_from_yoy.R
@@ -0,0 +1,47 @@
+#' @title Naive forecast
+#' @name naive
+#'
+#' @description Aplica o método naive para realizar projeção, cada valor previsto é igual ao último observado.
+#'
+#' @author Gabriel Bellé
+#'
+#' @param df Dataframe contendo a série limpa e organizada;
+#' @param end_projection Data contendo o último mês a ser projetado.
+#'
+#' @details
+#' O @param df de entrada deve conter pelo as colunas de:
+#' \code{date}: Data da observação:
+#' \code{vl}: valor da observação;
+#'
+#' @return Retorna o mesmo df de input, porém a projeção formalmente aplicada com o método naïve,
+#' e com a coluna forecast indicando quais observações são projeção.
+#'
+#' @examples
+#' \dontrun{
+#' naive(df = df_cleaned,
+#'       end_projection = '2026-12-01')
+#' }
+#'
+#' @export
+
+get_target_from_yoy <- function(df,
+                                yoy) {
+
+  last_vl <- df %>%
+    dplyr::mutate(
+      month = format(date, '%m') %>%
+        as.numeric(),
+      year = format(date, '%Y') %>%
+        as.numeric()
+    ) %>%
+    dplyr::filter(month == max(month)) %>%
+    dplyr::filter(year == max(year)) %>%
+    pluck('vl')
+
+  target <- c(last_vl, yoy/100 + 1) %>%
+    cumprod()
+
+  target <- target[2:length(target)]
+
+  return(target)
+}
