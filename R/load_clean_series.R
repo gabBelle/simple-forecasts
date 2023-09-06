214a1a8ea6299583a227a45d001340535eb66981
Gabriel Belle <g.belle@4intelligence.com.br>
2023-09-06 15:31:36 -0300

    UPDT: from 4macro pkg to FS pkg

--- R/load_clean_series.R
+++ R/load_clean_series.R
@@ -4,13 +4,13 @@
 #' @description Carrega e limpas as séries da FS.
 #'
 #' @param sid_vl Vetor chr com um ou mais SeriesID;
-#' @param auth_path Chr com caminho para o arquivo de autenticação do series.4macro;
 #' @param estimate bool indicando se deve baixar dado realizado ou projetado. Se NULL baixa ambas.
 #'
 #' @author Gabriel Bellé
 #'
-#' @details Baixa as séries da FS utilizando o pacote series.4macro e limpa elas
+#' @details Baixa as séries da FS utilizando o pacote fs-r-package e limpa elas
 #' para que fiquem no formato de {tibble}, empiladas e indicando qual valor é projeção.
+#' A autenticação deve ser feita no pacote fs-r-package.
 #'
 #' Se o parâmetro estimate for TRUE baixa apenas projeções, FALSE apenas dado realizado e NULL tudo disponível.
 #'
@@ -18,37 +18,30 @@
 #'
 #' @examples
 #' load_clean_series(sid_vl = c('BREMP0018000OOML', 'BREMP0018000SOML'),
-#'                   auth_path = 'User/auth.ini'
 #'                   estimate = TRUE)
 #'
 #' @export
 
-load_clean_series <- function(sid_vl, auth_path, estimate = NULL) {
+load_clean_series <- function(sid_vl, estimate = NULL) {
 
-  if(base::is.null(estimate)) {
-    query_data <- base::data.frame(sid = sid_vl, force = T)
-  } else {
-    query_data <- base::data.frame(sid = sid_vl, force = T, estimate = estimate)
+  series_fs <- fs4i::get_multi_series(series_code = sid_vl, estimate = T)[[1]] %>%
+    dplyr::rename(
+      sid = serie,
+      vl = value,
+      forecast = estimated
+    ) %>%
+    relocate(sid, date, forecast, vl) %>%
+    mutate(date = as.Date(str_sub(date, 1, 10)))
+
+  if(estimate %>% is.null()) {
+    cleaned_series <- series_fs
+  } else if(estimate) {
+    cleaned_series <- series_fs %>%
+      filter(forecast)
+  } else{
+    cleaned_series <- series_fs %>%
+      filter(!forecast)
   }
-
-  series_fs <- series.4macro::get_multi_series(query_data,
-                                               filepath = auth_path,
-                                               lang = "pt-br")
-
-  cleaned_series <- series_fs %>%
-    tibble::as_tibble() %>%
-    dplyr::select(c(sid, contents)) %>%
-    tidyr::unnest(contents) %>%
-    dplyr::mutate(
-      dt = dt %>%
-        base::as.Date(),
-      vl = base::as.numeric(vl),
-      sid = base::as.character(sid),
-      forecast = base::ifelse(lbl == '', F, T)) %>%
-    dplyr::arrange(sid, dt) %>%
-    dplyr::rename(date = dt) %>%
-    dplyr::select(-lbl) %>%
-    dplyr::relocate(forecast, .before = 'vl')
 
   return(cleaned_series)
 }
